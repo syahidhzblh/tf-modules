77f69c8a8a8ad6896a4accc80d25e161d7f5a5b6
syahidhzblh <syahidhizbullah17@gmail.com>
2023-11-15 14:27:23 +0700

    edit:uncomment-config

--- modules/production/ec2.tf
+++ modules/production/ec2.tf
@@ -1,11 +1,11 @@
-# resource "aws_instance" "ec2-prod" {
-#   ami                    = data.aws_ami.ubuntu.id
-#   instance_type          = "t2.small"
-#   vpc_security_group_ids = [aws_security_group.sg-prod-module.id]
-#   key_name               = var.keypair
+resource "aws_instance" "ec2-prod" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.small"
+  vpc_security_group_ids = [aws_security_group.sg-prod-module.id]
+  key_name               = var.keypair
 
-#   tags = {
-#     Name = "prod-server"
-#     Env  = "Production"
-#   }
-# }
+  tags = {
+    Name = "prod-server"
+    Env  = "Production"
+  }
+}
